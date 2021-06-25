03022e72a2649c4600cb2629651fba4b1ca33e3e
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-06-26 00:17:43 +0300

    heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/task-manager-angular'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/task-manager-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
